fc05d4ee74419421e60aebbfbbc89db39c3665f4
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-06-01 17:22:55 +0300

    File updated

--- tests/test_Parametrize_pytest.py
+++ tests/test_Parametrize_pytest.py
@@ -1,6 +1,7 @@
 import pytest
 
-                                        # 1 Параметризация с помощью фикстуры.
+
+# 1 Параметризация с помощью фикстуры.
 
 # Параметризация тестов с помощью фикстуры с  параметрами params=(количество объектов (тестов) тестирования) и
 # ids=(название тестов для удобства, которое должно быть равно количеству объектов (тестов) тестирования,
@@ -62,13 +63,50 @@ import pytest
 #    print("Входная строка: {0}\nВыходная строка: {1}\nОжидаемое значение: {2}".format(input, result, expected_output))
 #    assert result == expected_output
 
-                                        # 2 Параметризация с помощью pytest.mark.parametrize.
+# 2 Параметризация с помощью pytest.mark.parametrize.
 
+# 2.1
 # Если указать несколько меток с разными параметрами, то тест будет запущен со всеми возможными наборами параметров
 # (то есть мы имеем декартово произведение). Напишем простой тест, чтобы это продемонстрировать:
 
-@pytest.mark.parametrize("x", [1, 2, 3])
-@pytest.mark.parametrize("y", [10, 11])
-def test_multiply_params(x, y):
-   print("x: {0}, y: {1}".format(x, y))
-   assert True
+# @pytest.mark.parametrize("x", [1, 2, 3])
+# @pytest.mark.parametrize("y", [10, 11])
+# def test_multiply_params(x, y):
+#    print("x: {0}, y: {1}".format(x, y))
+#    assert True
+
+# 2.2
+# Точно так же нам доступны два варианта передачи ids в фикстуру — в виде имени функции или набора значений.
+# Для начала давайте посмотрим, как передаётся набор значений:
+
+# @pytest.mark.parametrize("x", [-1, 0, 1], ids=["negative", "zero", "positive"])
+# @pytest.mark.parametrize("y", [100, 1000], ids=["3 digit", "4 digit"])
+# def test_multiply_params(x, y):
+#    print("x: {0}, y: {1}".format(x, y))
+#    assert True
+
+
+# 2.3
+# Проделаем аналогичные операции с генератором имён параметров:
+# def ids_x(val):
+#     return "x=({0})".format(str(val))
+#
+#
+# def ids_y(val):
+#     return "y=({0})".format(str(val))
+#
+#
+# @pytest.mark.parametrize("x", [-1, 0, 1], ids=ids_x)
+# @pytest.mark.parametrize("y", [100, 1000], ids=ids_y)
+# def test_multiply_params(x, y):
+#     print("x: {0}, y: {1}".format(x, y))
+#     assert True
+
+# 2.4 Самостоятельныйтест:
+
+@pytest.mark.parametrize("r", [0, 255], ids=["0_red", "255_red"])
+@pytest.mark.parametrize("g", [0, 15, 150, 255], ids=["0_green", "15_green", "150_green", "255_green"])
+@pytest.mark.parametrize("b", [0, 30, 255], ids=["0_blue", "30_blue", "255_blue"])
+def test_color_picker(r, g, b):
+    print("r: {0}, g: {1}, b: {2}".format(r, g, b))
+    assert True
