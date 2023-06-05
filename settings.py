0fb7161e66978f14822c033837069591a7c10303
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-06-05 10:50:21 +0300

    File updated

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
-
 valid_email = 'fonovagafonov@yandex.ru'
 valid_password = '123456'
 
 invalid_email = '48234@yandex.ru'
 invalid_password = '1111111111'
+
+
--- tests/test_Parametrize_pytest.py
+++ tests/test_Parametrize_pytest.py
@@ -205,61 +205,95 @@ def chinese_chars():
 
 def special_chars():
    return '|\\/!@#$%^&*()-_=+`~?"№;:[]{}'
-
-
-@pytest.fixture(autouse=True)
-def get_api_key():
-   #""" Проверяем, что запрос api-ключа возвращает статус 200 и в результате содержится слово key"""
-
-   # Отправляем запрос и сохраняем полученный ответ с кодом статуса в status, а текст ответа в result
-   status, pytest.key = pf.get_api_key(valid_email, valid_password)
-
-   # Сверяем полученные данные с нашими ожиданиями
-   assert status == 200
-   assert 'key' in pytest.key
-
-   yield
-
-
-@pytest.mark.parametrize("filter",
-                        [
-                            generate_string(255)
-                            , generate_string(1001)
-                            , russian_chars()
-                            , russian_chars().upper()
-                            , chinese_chars()
-                            , special_chars()
-                            , 123
-                        ],
-                        ids =
-                        [
-                            '255 symbols'
-                            , 'more than 1000 symbols'
-                            , 'russian'
-                            , 'RUSSIAN'
-                            , 'chinese'
-                            , 'specials'
-                            , 'digit'
-                        ])
-def test_get_all_pets_with_negative_filter(filter):
-   pytest.status, result = pf.get_list_of_pets(pytest.key, filter)
-
-   # Проверяем статус ответа
-   assert pytest.status == 400
-
-
-@pytest.mark.parametrize("filter",
-                        ['', 'my_pets'],
-                        ids=['empty string', 'only my pets'])
-def test_get_all_pets_with_valid_key(filter):
-   pytest.status, result = pf.get_list_of_pets(pytest.key, filter)
-
-   # Проверяем статус ответа
-   assert pytest.status == 200
-   assert len(result['pets']) > 0
+#
+#
+# @pytest.fixture(autouse=True)
+# def get_api_key():
+#    #""" Проверяем, что запрос api-ключа возвращает статус 200 и в результате содержится слово key"""
+#
+#    # Отправляем запрос и сохраняем полученный ответ с кодом статуса в status, а текст ответа в result
+#    status, pytest.key = pf.get_api_key(valid_email, valid_password)
+#
+#    # Сверяем полученные данные с нашими ожиданиями
+#    assert status == 200
+#    assert 'key' in pytest.key
+#
+#    yield
+#
+#
+# @pytest.mark.parametrize("filter",
+#                         [
+#                             generate_string(255)
+#                             , generate_string(1001)
+#                             , russian_chars()
+#                             , russian_chars().upper()
+#                             , chinese_chars()
+#                             , special_chars()
+#                             , 123
+#                         ],
+#                         ids =
+#                         [
+#                             '255 symbols'
+#                             , 'more than 1000 symbols'
+#                             , 'russian'
+#                             , 'RUSSIAN'
+#                             , 'chinese'
+#                             , 'specials'
+#                             , 'digit'
+#                         ])
+# def test_get_all_pets_with_negative_filter(filter):
+#    pytest.status, result = pf.get_list_of_pets(pytest.key, filter)
+#
+#    # Проверяем статус ответа
+#    assert pytest.status == 400
+#
+#
+# @pytest.mark.parametrize("filter",
+#                         ['', 'my_pets'],
+#                         ids=['empty string', 'only my pets'])
+# def test_get_all_pets_with_valid_key(filter):
+#    pytest.status, result = pf.get_list_of_pets(pytest.key, filter)
+#
+#    # Проверяем статус ответа
+#    assert pytest.status == 200
+#    assert len(result['pets']) > 0
 
 
                                              #  27.3. Сложная параметризация тестов
+
+def is_age_valid(age):
+   # Проверяем, что возраст - это число от 1 до 49 и целое
+   return age.isdigit() \
+          and 0 < int(age) < 50 \
+          and float(age) == int(age)
+#
+#
+# @pytest.mark.parametrize("name"
+#    , ['', generate_string(255), generate_string(1001), russian_chars(), russian_chars().upper(), chinese_chars(), special_chars(), '123']
+#    , ids=['empty', '255 symbols', 'more than 1000 symbols', 'russian', 'RUSSIAN', 'chinese', 'specials', 'digit'])
+# @pytest.mark.parametrize("animal_type"
+#    , ['', generate_string(255), generate_string(1001), russian_chars(), russian_chars().upper(), chinese_chars(), special_chars(), '123']
+#    , ids=['empty', '255 symbols', 'more than 1000 symbols', 'russian', 'RUSSIAN', 'chinese', 'specials', 'digit'])
+# @pytest.mark.parametrize("age"
+#    , ['', '-1', '0', '1', '100', '1.5', '2147483647', '2147483648', special_chars(), russian_chars(), russian_chars().upper(), chinese_chars()]
+#    , ids=['empty', 'negative', 'zero', 'min', 'greater than max', 'float', 'int_max', 'int_max + 1', 'specials', 'russian', 'RUSSIAN', 'chinese'])
+# def test_add_new_pet_simple(name, animal_type, age):
+#    """Проверяем, что можно добавить питомца с различными данными"""
+#
+#    # Добавляем питомца
+#    pytest.status, result = pf.create_pet_simple(pytest.key, name, animal_type, age)
+#
+#    # Сверяем полученный ответ с ожидаемым результатом
+#    if name == '' or animal_type == '' or is_age_valid(age):
+#        assert pytest.status == 400
+#    else:
+#        assert pytest.status == 200
+#        assert result['name'] == name
+#        assert result['age'] == age
+#        assert result['animal_type'] == animal_type
+
+
+                            # 27.4. Автоматизация с параметризацией для негативных тестов
 
 def is_age_valid(age):
    # Проверяем, что возраст - это число от 1 до 49 и целое
@@ -291,3 +325,18 @@ def test_add_new_pet_simple(name, animal_type, age):
        assert result['name'] == name
        assert result['age'] == age
        assert result['animal_type'] == animal_type
+
+@pytest.mark.parametrize("name", [''], ids=['empty'])
+@pytest.mark.parametrize("animal_type", [''], ids=['empty'])
+@pytest.mark.parametrize("age",
+                        ['', '-1', '0', '100', '1.5', '2147483647', '2147483648', special_chars(), russian_chars(),
+                         russian_chars().upper(), chinese_chars()]
+   , ids=['empty', 'negative', 'zero', 'greater than max', 'float', 'int_max', 'int_max + 1', 'specials',
+          'russian', 'RUSSIAN', 'chinese'])
+def test_add_new_pet_simple_negative(name, animal_type, age):
+
+   # Добавляем питомца
+   pytest.status, result = pf.create_pet_simple(pytest.key, name, animal_type, age)
+
+   # Сверяем полученный ответ с ожидаемым результатом
+   assert pytest.status == 400
